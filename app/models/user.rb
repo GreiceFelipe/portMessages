4b9195ae94f11b65edd73cf80809496d4a59ee08
greice <greice.n.felipe@gmail.com>
2023-01-16 23:18:39 -0300

    Add user and employess controller

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: { employee: 0, admin: 1 }
+end
