bbdec0e47ebe60e3a68d605b2f707018723ef2aa
greice <greice.n.felipe@gmail.com>
2023-04-13 12:28:11 -0300

    Mudancas

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
+  resources :employees, only: [:new, :create, :edit, :update, :index, :destroy]
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
 
-  get '/employees/:id', to: 'employees#show'
-  get '/employees', to: 'employees#index'
 end
